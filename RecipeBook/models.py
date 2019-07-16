b9dbf1be6074a1fa80e8596d3309d1a0e31651a0
castertr0y357 <castertr0y357@hotmail.com>
2019-07-16 01:02:43 -0400

    Initial commit with model structure and basic views

--- RecipeBook/models.py
+++ RecipeBook/models.py
@@ -13,8 +13,10 @@ class Recipe(models.Model):
     id = models.AutoField(primary_key=True)
     recipe_name = models.CharField(max_length=100)
     ingredients_list = models.TextField()
+    serving_size = models.IntegerField()
     directions = models.TextField()
     recipe_url = models.URLField()
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, null=True, blank=True)
 
     def __str__(self):
         return self.recipe_name
--- RecipeBook/views.py
+++ RecipeBook/views.py
@@ -1,3 +1,29 @@
 from django.views import generic
+from .models import Category, Recipe
 
-# Create your views here.
+
+class MainView(generic.ListView):
+    model = Category
+    template_name = 'RecipeBook/main_page.html'
+    context_object_name = 'categories'
+    queryset = Category.objects.all().order_by('category_name')
+
+
+class CategoryView(generic.DetailView):
+    model = Category
+    context_object_name = 'category_recipe_list'
+    queryset = None
+    slug_field = 'category_name'
+    slug_url_kwarg = 'category_name'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(CategoryView, self).get_context_data()
+        category = self.object
+
+        recipes = Recipe.objects.filter(category=category)
+
+        context['recipes'] = recipes
+
+        return context
+
+
